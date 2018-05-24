fbd234e3236b241531ebb113ad81801fdd7d4f13
Hiyro92 <Hiyro92@gmail.com>
2018-05-24 21:48:13 +0200

    Neues Projekt add Tim

--- src/de/LastCorp/GitTest.java
+++ src/de/LastCorp/GitTest.java
@@ -17,7 +17,7 @@ public class GitTest {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        DataBean tim = new DataBean("Tim", "Schicho");
     }
     
 }
