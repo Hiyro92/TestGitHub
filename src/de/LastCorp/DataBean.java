a428d5da09c09b20434dcca344f9812d18242bea
Hiyro92 <Hiyro92@gmail.com>
2018-05-24 21:32:42 +0200

    Neues Projekt

---
+++ src/de/LastCorp/DataBean.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.LastCorp;
+
+/**
+ *
+ * @author Adrian
+ */
+public class DataBean {
+    String firstName;
+    String lastName;
+
+    public DataBean(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+}
