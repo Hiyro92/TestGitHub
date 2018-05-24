42a4aa643a1e6078d83093a5d1492f2b1b5b9f55
Hiyro92 <Hiyro92@gmail.com>
2018-05-24 21:29:07 +0200

    Neues Projekt

---
+++ src/de/LastCorp/GitTest.java
@@ -0,0 +1,23 @@
+package de.LastCorp;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/**
+ *
+ * @author Adrian
+ */
+public class GitTest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
